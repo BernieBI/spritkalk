dc836a9f4fa554929377b7d214e92d6f31afa699
BernieBI <matslovstrandberntsen@outlook.com>
2016-11-09 21:56:45 +0100

    Ryddet i html og js. henting av data virker

--- javascript/spritkalk.js
+++ javascript/spritkalk.js
@@ -1,18 +1,9 @@
 //https://www.vinmonopolet.no/medias/sys_master/products/products/hbc/hb0/8834253127710/produkter.csv
 
-document.getElementById("import").onclick = getData;
+document.getElementById("btn").onclick = getData;
 var xmlhttp
 
-<<<<<<< HEAD
 function getData() {
-=======
-function getData(){
-
-  xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = statusforandring;
-  xmlhttp.open("GET", "../resources/produkter.csv", true);
-  xmlhttp.send();
->>>>>>> 73295456588ec5e4a81665ec897385cd4f06ccda
 
     xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = statusforandring;
@@ -21,7 +12,6 @@ function getData(){
 }
 
 function statusforandring() {
-    //ø = &#216;
     var bildeUrl = "https://bilder.vinmonopolet.no/cache/1200x1200-0/";
     var bildeUrlSlutt = "-1.jpg";
     if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
