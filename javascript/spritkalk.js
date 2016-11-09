6ff22fe81e0ea01ff98f74c6e3414c3fad71678e
Jim-Alexander Berger Gundersen <alexandergundersen@yahoo.no>
2016-11-09 22:00:26 +0100

    backup før papa

--- javascript/spritkalk.js
+++ javascript/spritkalk.js
@@ -1,28 +1,34 @@
 //https://www.vinmonopolet.no/medias/sys_master/products/products/hbc/hb0/8834253127710/produkter.csv
 
-document.getElementById("btn").onclick = getData;
+document.getElementById("import").onclick = getData;
 var xmlhttp
 
-function getData() {
+function getData(){
 
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = statusforandring;
-    xmlhttp.open("GET", "../resources/produkter.csv", true);
-    xmlhttp.send();
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = statusforandring;
+  xmlhttp.open("GET", "../resources/produkter.csv", true);
+  xmlhttp.send();
+
 }
 
 function statusforandring() {
-    var bildeUrl = "https://bilder.vinmonopolet.no/cache/1200x1200-0/";
-    var bildeUrlSlutt = "-1.jpg";
+
     if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
 
         var data = xmlhttp.responseText;
         var produkt = data.split("\n");
-        for (var i = 0; i < /*produkt.length*/ 50; i++) {
-            var info = produkt[i].split(";");
-            document.getElementById("output").innerHTML += "<img scr =" + bildeUrl + info[0] + bildeUrlSlutt + ">" + "Navn:" + info[2] + " Pris:" + info[4] + "<br />";
-        }
-    }
+        console.log("started");
 
-    console.log("finished");
+        for (var i = 0; i < produkt.length; i++) {
+
+            var info = produkt[i].split(";");
+
+            document.getElementById("output").innerHTML += "Navn:" + info[2] + " Pris:" + info[4] + "<br />";
+
+
+
+        }
+        console.log("finished");
+    }
 }
