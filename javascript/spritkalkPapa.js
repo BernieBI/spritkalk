dc194537f1c6cd1802815eafc0b8ce810de2bf5e
Jim-Alexander Berger Gundersen <alexandergundersen@yahoo.no>
2016-11-09 23:24:48 +0100

    la til kommentarer i spritkalkpapa js fila

--- javascript/spritkalkPapa.js
+++ javascript/spritkalkPapa.js
@@ -1,10 +1,13 @@
 //https://www.vinmonopolet.no/medias/sys_master/products/products/hbc/hb0/8834253127710/produkter.csv
 
+
+//tastetrykk
 document.getElementById("btn").onclick = getData;
 var xmlhttp
 
 function getData() {
 
+//Henter data
     xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = statusforandring;
     xmlhttp.open("GET", "../resources/produkter.csv", true);
@@ -12,22 +15,32 @@ function getData() {
 
 }
 
+
 function statusforandring() {
+
 
     if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
 
-        var data = xmlhttp.responseText;
-
+//bruker papa parse til å kjøre gjennom hver linje i csv fila uten at det går utover chrome
         Papa.parse(xmlhttp.responseText, {
+          //Betyr at den jobber selvstendig og typ jobber på egen "thread" så man fortsatt kan bruke html knapper etc..
             worker: true,
+            //Betyr at den gir objektet sine verdier navn utifra øverste raden i dokumentet i stedet for 1,2,3 så får den varenavn, aroma, etc..
             header: true,
+
+            //funksjonen som skal kjøre på hver linje i csv fila
             step: function(row) {
-                console.log("Row:", row.data);
+                //row.data[0] er objektet vi jobber med
+
+                //lager ett bildeelement utifra url'en til bildet hentet frem fra vinmonopolet sine sider
                 var src = "https://bilder.vinmonopolet.no/cache/1200x1200-0/" + row.data[0].Varenummer + '-1.jpg',
                     img = document.createElement('img');
                 img.src = src;
+                //legger det til i div'en output
                 document.getElementById("output").appendChild(img);
             },
+
+            //funksjonen som skjører når hele fila har blitt kjørt igjennom
             complete: function() {
                 console.log("All done!");
             }
