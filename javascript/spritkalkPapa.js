e20b5fae5b6509613861cea792934765011bcb9b
BernieBI <matslovstrandberntsen@outlook.com>
2016-11-10 09:23:01 +0100

    la til filter på alk og pris

--- javascript/spritkalkPapa.js
+++ javascript/spritkalkPapa.js
@@ -7,7 +7,6 @@ document.getElementById("alkTo").oninput = displayalk;
 document.getElementById("priceFrom").oninput = displayprice;
 document.getElementById("priceTo").oninput = displayprice;
 
-var xmlhttp
 
 function getData() {
 
@@ -19,10 +18,13 @@ function getData() {
 }
 
 function statusforandring() {
-
     if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
 
         var data = xmlhttp.responseText;
+        var priceto = parseInt(document.getElementById("priceTo").value);
+        var pricefrom = parseInt(document.getElementById("priceFrom").value);
+        var alkto = parseInt(document.getElementById("alkTo").value);
+        var alkfrom = parseInt(document.getElementById("alkFrom").value);
 
         Papa.parse(xmlhttp.responseText, {
             worker: true,
@@ -30,20 +32,27 @@ function statusforandring() {
             step: function(row) {
                 console.log("Row:", row.data);
 
-                produktDiv = document.createElement('div');
-                produktDiv.className = "produktDiv";
-                produktDiv.id = row.data[0].Varenummer + "div";
-                document.getElementById("output").appendChild(produktDiv);
+                if (pricefrom <= row.data[0].Pris && priceto >= row.data[0].Pris) {
+                    if (alkfrom <= row.data[0].Pris && alkto >= row.data[0].Alkohol) {
+                        produktDiv = document.createElement('div');
+                        produktDiv.className = "produktDiv";
+                        produktDiv.id = row.data[0].Varenummer + "div";
+                        document.getElementById("output").appendChild(produktDiv);
 
-                var src = "https://bilder.vinmonopolet.no/cache/1200x1200-0/" + row.data[0].Varenummer + '-1.jpg',
-                    img = document.createElement('img');
-                img.src = src;
-                document.getElementById(row.data[0].Varenummer + "div").appendChild(img);
+                        var src = "https://bilder.vinmonopolet.no/cache/1200x1200-0/" + row.data[0].Varenummer + '-1.jpg',
+                            img = document.createElement('img');
+                        img.src = src;
+                        document.getElementById(row.data[0].Varenummer + "div").appendChild(img);
 
-                produktH3 = document.createElement('h3');
-                produktH3.innerHTML = row.data[0].Varenavn;
-                document.getElementById(row.data[0].Varenummer + "div").appendChild(produktH3);
+                        produktH3 = document.createElement('h3');
+                        produktH3.innerHTML = row.data[0].Varenavn;
+                        document.getElementById(row.data[0].Varenummer + "div").appendChild(produktH3);
 
+                        produktP = document.createElement('p');
+                        produktP.innerHTML = row.data[0].Alkohol + "% <br />" + "Pris:" + row.data[0].Pris;
+                        document.getElementById(row.data[0].Varenummer + "div").appendChild(produktP);
+                    }
+                }
             },
             complete: function() {
                 console.log("All done!");
@@ -54,21 +63,18 @@ function statusforandring() {
 }
 
 function displayalk() {
-    var alkto = document.getElementById("alkTo").value;
     var alkfrom = document.getElementById("alkFrom").value;
-    if (alkto < alkfrom) {
-        alkto = alkfrom;
-    }
+    var alkto = document.getElementById("alkTo").value;
+
     document.getElementById("alkFromNum").innerHTML = alkfrom + "%";
     document.getElementById("alkToNum").innerHTML = alkto + "%";
 }
 
 function displayprice() {
-    var priceto = document.getElementById("priceTo").value;
     var pricefrom = document.getElementById("priceFrom").value;
-    if (priceto < pricefrom) {
-        priceto = pricefrom;
-    }
+    var priceto = document.getElementById("priceTo").value;
+
+
     document.getElementById("priceFromNum").innerHTML = pricefrom + " kr";
     document.getElementById("priceToNum").innerHTML = priceto + " kr";
 }
